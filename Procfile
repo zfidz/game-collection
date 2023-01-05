352f44de38806a8e5fc1e08f66dc8c19447767c9
Zack Fiddler <zack.fiddler@me.com>
2023-01-05 15:51:20 -0800

    add requirement.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gamecollector.wsgi
--- gamecollector/settings.py
+++ gamecollector/settings.py
@@ -128,3 +128,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+import django_on_heroku
+django_on_heroku.settings(locals())
